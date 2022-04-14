dd681edad5f0972a8bd81ed256a8d5a14139d5b7
heartandkarma <heartandkarma@gmail.com>
2022-04-14 11:59:48 -0700

    got test 4 working

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -22,7 +22,14 @@ test("renders ONE error message if user enters less then 5 characters into first
   ).toBeInTheDocument();
 });
 
-test("renders THREE error messages if user enters no values into any fields.", async () => {});
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  render(<ContactForm />);
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  userEvent.click(submitButton);
+  await waitFor(() => {
+    expect(screen.getAllByText(/Error/i).length).toBe(3);
+  });
+});
 
 test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
 
